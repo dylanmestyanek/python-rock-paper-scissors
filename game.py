6269fd149db7377e75d13380b3db5a2212da4a72
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-11 12:28:15 -0600

    Initial commit

---
+++ game.py
@@ -0,0 +1,8 @@
+# Create a rock paper scissors game in Python
+
+# Player should be able to type r, p, or s
+# Computer will pick r, p or s
+
+# Game will print out the results and keep track of wins, losses and ties
+
+# Type q to quit
